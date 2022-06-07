09b68f217b5e8aa2843609a9d1dae75a1265e0e7
Michael Dickens <dickens.michael64@gmail.com>
2022-06-07 11:09:02 -0500

    finished all exercises excpet last for area

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -23,15 +23,32 @@ public class ConsoleExercises {
 //        String userInput = scanner.nextLine();
 //        System.out.printf("Your integer is: %s", userInput);
 
-        String wordOne;
-        String wordTwo;
-        String wordThree;
+//        String wordOne;
+//        String wordTwo;
+//        String wordThree;
+//
+//        System.out.println("Enter three words: ");
+//        wordOne = scanner.next();
+//        wordTwo = scanner.next();
+//        wordThree = scanner.next();
+//        System.out.println("Your three words are: \n" + wordOne +"\n" + wordTwo + "\n" + wordThree);
 
-        System.out.println("Enter three words: ");
-        wordOne = scanner.next();
-        wordTwo = scanner.next();
-        wordThree = scanner.next();
-        System.out.println("Your three words are: \n" + wordOne +"\n" + wordTwo + "\n" + wordThree);
+//        String inputSentence;
+//        System.out.println("Please type a sentence.");
+//        inputSentence = scanner.nextLine();
+//        System.out.println("This is your sentence: "  + inputSentence);
+
+//           --------   3.  Calculate perimeter and area  ------
+
+
+        System.out.println("Please enter the length of your room.");
+        String length = scanner.nextLine();
+        System.out.println("Please enter the width of your room.");
+        String width = scanner.nextLine();
+        int perimeter = (Integer.parseInt(length) + Integer.parseInt(width)) * 2;
+        System.out.printf("The perimeter of your room is %d%n", perimeter);
+
+//          -----  do area, too -----
 
 
     }
