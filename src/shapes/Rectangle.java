5dd0029f857fc6e67895dec7f50c58e9326f78ce
Michael Dickens <dickens.michael64@gmail.com>
2022-06-15 19:00:58 -0300

    pushing files for Interfaces and Abstract Exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,25 +1,28 @@
 package shapes;
 
 public class Rectangle {
-
-    protected double length;
-    protected double width;
-
-    public Rectangle(){}
-
-    public Rectangle(double length, double width) {
-        this.length = length;
-        this.width = width;
-    }
-
-    public double getArea() {
-        return area = length * width;
-        System.out.println("The area is: " + getArea());
-    }
-
-    public double getPerimeter() {
-        return perimeter = 2 * length + 2 * width;
-        System.out.println("The perimeter is: " + getPerimeter());
-    }
-
+//    private double side;
+//    double area;
+//    double perimeter;
+//
+//    protected double length;
+//    protected double width;
+//    public Rectangle() {}
+//
+//    public Rectangle(double length, double width) {
+//        this.length = length;
+//        this.width = width;
+//
+//
+//        public double getArea(){
+//            System.out.println("The area is: " + area);
+//            return Math.pow(side, width);
+//        }
+//
+//        public double getPerimeter() {
+//            System.out.println("The perimeter is: " + perimeter);
+//            return Math.pow((2 * length) + (2 * width);
+//
+//        }
+//    }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,5 +1,20 @@
 package shapes;
 
 public class Square extends Rectangle {
+//
+//
+//public Square(double side) {
+//    super(side, side);
+//    this.side = side;
+//}
+//
+//public double getArea() {
+//    return side * side;
+//}
+//
+//public double getPerimeter() {
+//    return side + side + side + side;
+//}
+
 
 }
