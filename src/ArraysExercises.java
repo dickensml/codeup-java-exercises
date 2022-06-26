49d8fc66bb504f346846943b6493175f5f6b6d2f
Michael Dickens <dickens.michael64@gmail.com>
2022-06-26 16:53:39 -0500

    rework Arrays Ex 1

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -3,28 +3,30 @@ import java.util.Arrays;
 
 public class ArraysExercises {
 
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
 
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(numbers));
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(Arrays.toString(numbers));
 
-//        public static Person[] addPerson(Person newPerson, Person[] persons) {
-//            Person[] newPersonArray;
-//            new PersonArray = Arrays.copyOf((persons, person.length + 1);
-
+        public static Person[] addPerson(Person newPerson, Person[] persons) {
+            Person[] newPersonArray;
+            newPersonArray = Arrays.copyOf(persons, persons.length + 1);
+            newPersonArray[newPersonArray.length-1] = newPerson;
+            return newPersonArray;
         }
 
-//        Person[] persons;
-//        persons = new Person[3];
-//        persons[0] = new Person("Justin");
-//        persons[1] = new Person("Dane");
-//        persons[2] = new Person("Javier");
-//
-//        for (Person person: persons) {
-//            person.sayHello();
-//        }
+    public static void main(String[] args) {
+        Person[] persons;
+        persons = new Person[3];
+        persons[0] = new Person("Justin");
+        persons[1] = new Person("Dane");
+        persons[2] = new Person("Javier");
+
+        for (Person person: persons) {
+            person.sayHello();
+        }
 
 
-//    }
+    }
 }
 
