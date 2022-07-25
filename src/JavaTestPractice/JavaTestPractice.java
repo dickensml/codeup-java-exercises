1dd16883896c47d745492181c05b025ae68ed2c1
Michael Dickens <dickens.michael64@gmail.com>
2022-07-24 19:47:48 -0500

    uncommited stuff

--- src/JavaTestPractice/JavaTestPractice.java
+++ src/JavaTestPractice/JavaTestPractice.java
@@ -1,5 +1,10 @@
 package JavaTestPractice;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class JavaTestPractice {
 
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
