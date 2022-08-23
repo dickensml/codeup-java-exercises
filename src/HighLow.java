8c6665d2b7b99bb05229585589a160276c9391c8
Michael Dickens <dickens.michael64@gmail.com>
2022-08-22 22:34:37 -0500

    reworking HighLow

---
+++ src/HighLow.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class HighLow {
+
+    public static void init() {
+        System.out.println("Welcome to the High / Low Game!");
+
+        int randomNum = (int) (Math.random() * 100 +1);
+
+        int randomNumber = (int)randomNum;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Guess a number between 1 and 100");
+        int guess = Integer.parseInt(scanner.next());
+
+        if (guess == randomNum) {
+            System.out.println("GOOD GUESS!");
+        } else if (guess < randomNum) {
+            System.out.println("HIGHER");
+        }else {
+            System.out.println("LOWER");
+        }
+//        System.out.println(randomNum);
+    }
+
+    public static void main(String[] args) {
+        init();
+    }
+
+
+
+}
